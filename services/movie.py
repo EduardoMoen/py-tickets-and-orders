1f838d1e07c3c65bfefc72faca53df56566f053f
Eduardo Doerner <eduardodoerner@gmail.com>
2025-10-22 10:46:26 -0300

    Solution

--- services/movie.py
+++ services/movie.py
@@ -7,8 +7,8 @@ from db.models import Movie
 def get_movies(
     genres_ids: list[int] = None,
     actors_ids: list[int] = None,
-    title: Optional[QuerySet[Movie]] = None
-) -> QuerySet:
+    title: Optional[str] = None
+) -> QuerySet[Movie]:
     queryset = Movie.objects.all()
 
     if title:
